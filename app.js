55c38b9c5261253214a1ffc458ae6f9d0f50853a
Steveen <steveen.echeverri@gmail.com>
2020-10-12 22:08:18 -0500

    First schema and a first set of GraphQL

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
+
+app.use('/graphql', graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery {
+            event: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        event: () => {
+            return ['Night coding', 'Watching tv', 'Sailing'];
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
+}));
 
 app.get('/', (req, res, next) => {
     res.send("Working");
