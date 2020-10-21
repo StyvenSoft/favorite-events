6423cc5d41ad4177a8d133d8556577c89a31e878
Steveen <steveen.echeverri@gmail.com>
2020-10-20 19:46:08 -0500

    Created middleware for is auth

---
+++ middleware/is-auth.js
@@ -0,0 +1,8 @@
+module.exports = (req, res, next) => {
+    const authHeader = req.get('Authorization');
+    if(!authHeader) {
+        req.isAuth = false;
+        return next();
+    }
+    const token = authHeader.split(' ')[1];
+}
