4efcc2662e9e27fbeb10b3319434f4069230fbca
Steveen <steveen.echeverri@gmail.com>
2020-11-30 19:14:39 -0500

    Complete implementation basic modal for events

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -6,8 +6,8 @@ const modal = props => (
         <header className="modal__header">{props.title}</header>
         <section className="modal__content">{props.children}</section>
         <section className="modal__actions">
-            {props.canCancel && <button className="btn">Cancel</button>}
-            {props.canConfirm && <button className="btn">Confirm</button>}
+            {props.canCancel && <button className="btn" onClick={props.onCancel}>Cancel</button>}
+            {props.canConfirm && <button className="btn" onClick={props.onConfirm}>Confirm</button>}
         </section>
     </div>
 );
