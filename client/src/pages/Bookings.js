4f03fab9c3924aa551889071b2655bf8e88bb7be
Steveen <steveen.echeverri@gmail.com>
2020-12-10 00:19:59 -0500

    Created cancel booking for Auth user

--- client/src/pages/Bookings.js
+++ client/src/pages/Bookings.js
@@ -57,13 +57,54 @@ class BookingsPage extends Component {
             });
     };
 
+    deleteBookingHandler = bookingId => {
+        this.setState({ isLoading: true });
+        const requestBody = {
+            query: `
+                mutation {
+                    cancelBooking(bookingId: "${bookingId}") {
+                    _id
+                    title
+                    }
+                }
+            `
+        };
+
+        fetch('http://localhost:4000/graphql', {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer ' + this.context.token
+            }
+        })
+            .then(res => {
+                if (res.status !== 200 && res.status !== 201) {
+                    throw new Error('Failed!');
+                }
+                return res.json();
+            })
+            .then(resData => {
+                this.setState(prevState => {
+                    const updateBookings = prevState.bookings.filter(booking => {
+                        return booking._id !== bookingId;
+                    });
+                    return { bookings: updateBookings, isLoading: false };
+                })
+            })
+            .catch(err => {
+                console.log(err);
+                this.setState({ isLoading: false });
+            });
+    }
+
     render() {
         return (
             <React.Fragment>
                 {this.state.isLoading ? (
                     <Spinner />
                 ) : (
-                        <BookingList bookings={this.state.bookings} />
+                        <BookingList bookings={this.state.bookings} onDelete={this.deleteBookingHandler} />
                     )}
             </React.Fragment>
         )
